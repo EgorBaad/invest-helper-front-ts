5a327f0496a4f3f41fbb4a7cde4395263c7c31ab
Egor <bd412997@gmail.com>
2023-09-29 21:07:02 +0300

    Added empty token alert on home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,12 @@
+import { Alert, Box } from "@mui/material";
+import Cookies from "universal-cookie"
+
 export function Home () {
+    const cookies = new Cookies();
+
     return (
-        <>{}</>
+        <Box>
+            {!cookies.get("api-token") && <Alert severity="info">Your token is empty. Please set your token in the settings before starting.</Alert>}
+        </Box>
     )
 }
