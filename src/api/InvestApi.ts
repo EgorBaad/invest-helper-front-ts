3a778a6f8301e4aeed566a5df7022df10a169c1b
Egor <bd412997@gmail.com>
2023-09-29 20:48:11 +0300

    token sets to cookie

--- src/api/InvestApi.ts
+++ src/api/InvestApi.ts
@@ -1,8 +1,8 @@
 export class InvestApi {
     static url: string = "http://localhost:8888/";
 
-    static async setToken(token: string) {
-        const req = await fetch(this.url + "token/set?" + new URLSearchParams({
+    static async testToken(token: string) {
+        const req = await fetch(this.url + "token/test?" + new URLSearchParams({
             token: token
         }), {
             method: "PUT"
--- src/components/Header/Settings/Settings.tsx
+++ src/components/Header/Settings/Settings.tsx
@@ -1,12 +1,21 @@
 /** @jsxImportSource @emotion/react */
 //eslint-disable-next-line
 import { jsx } from "@emotion/react";
-import { Box, Button, Drawer, TextField } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Drawer,
+  Snackbar,
+  TextField,
+} from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import React from "react";
 import styled from "@emotion/styled";
 import { InvestApi } from "../../../api/InvestApi";
 import { useFormik } from "formik";
+import Cookies from "universal-cookie";
 
 const Expander = styled(Button)`
   position: absolute;
@@ -18,27 +27,49 @@ const DrawerArea = styled(Box)`
   width: 500px;
   padding: 12px;
 
-  div {
+  div.MuiTextField-root {
     width: 100%;
   }
+`;
+
+const Loader = styled(CircularProgress)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
 export default function Settings() {
   const [isDrawn, setIsDrawn] = React.useState(false);
   const [tokenError, setTokenError] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const toggleDrawer = (open: boolean) => () => {
     setIsDrawn(open);
   };
+  const cookie = new Cookies();
 
   const setToken = (token: string) => {
-    InvestApi.setToken(token).catch(() => {
-      setTokenError("Failed to set token");
-    });
+    setLoading(true);
+    InvestApi.testToken(token)
+      .then((result) => {
+        if (result && result !== "false") {
+          cookie.set("api-token", token);
+          setSuccess(true);
+          setTokenError("");
+        } else {
+          setTokenError("Failed to set token");
+        }
+        setLoading(false);
+      })
+      .catch(() => {
+        setTokenError("Failed to set token");
+        setLoading(false);
+      });
   };
 
   const formik = useFormik({
     initialValues: {
-      token: "",
+      token: cookie.get("api-token"),
     },
     onSubmit: (values) => {
       setToken(values.token);
@@ -57,7 +88,21 @@ export default function Settings() {
       </Expander>
       <Drawer anchor={"right"} open={isDrawn} onClose={toggleDrawer(false)}>
         <DrawerArea role="presentation">
-          <form onSubmit={formik.handleSubmit}>
+          {loading && <Loader color="primary" />}
+          <Snackbar
+            open={success}
+            autoHideDuration={2000}
+            onClose={() => setSuccess(false)}
+            anchorOrigin={{vertical: "bottom", horizontal: "right"}}
+          >
+            <Alert onClose={() => setSuccess(false)} severity={"success"}>
+              Settings saved!
+            </Alert>
+          </Snackbar>
+          <form
+            onSubmit={formik.handleSubmit}
+            css={loading && { opacity: "50%" }}
+          >
             <Box>
               <h1>Settings</h1>
             </Box>
