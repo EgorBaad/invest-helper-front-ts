c7542c7f10f3988eaf54b3c1e04b9a336458bee0
Egor <bd412997@gmail.com>
2023-09-29 21:16:38 +0300

    getAccounts and getPortfolio methods implemented

--- src/api/InvestApi.ts
+++ src/api/InvestApi.ts
@@ -1,12 +1,37 @@
 export class InvestApi {
-    static url: string = "http://localhost:8888/";
+  static url: string = "http://localhost:8888/";
 
-    static async testToken(token: string) {
-        const req = await fetch(this.url + "token/test?" + new URLSearchParams({
-            token: token
-        }), {
-            method: "PUT"
-        });
-        return await req.text();
-    }
+  static async testToken(token: string) {
+    const req = await fetch(
+      this.url +
+        "token/test?" +
+        new URLSearchParams({
+          token: token,
+        }),
+      {
+        method: "PUT",
+      }
+    );
+    return await req.text();
+  }
+
+  static async getAccounts(token: string) {
+    const req = await fetch(this.url + "account/all", {
+      method: "GET",
+      headers: {
+        token: token,
+      },
+    });
+    return await req.json();
+  }
+
+  static async getPortfolio(token: string, accountId: string) {
+    const req = await fetch(this.url + "portfolio/" + accountId, {
+      method: "GET",
+      headers: {
+        token: token,
+      },
+    });
+    return await req.json();
+  }
 }
